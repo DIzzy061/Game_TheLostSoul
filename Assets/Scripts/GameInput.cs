98b2f6fc053f4378f801ea2c3da6855492a0911f
Chaly_Eduard <chaliedik9@gmail.com>
2025-06-05 01:19:08 +0700

    First commit

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    public static GameInput Instance { get; private set; }
+
+    private PlayerInputActions PlayerInputActions;
+
+    private void Awake()
+    {
+        Instance = this;
+        PlayerInputActions = new PlayerInputActions();
+        PlayerInputActions.Enable();
+    }
+
+    public Vector2 GetMovementVector()
+    {
+        Vector2 inputVector = PlayerInputActions.Player.Move.ReadValue<Vector2>();
+
+        return inputVector;
+    }
+}
