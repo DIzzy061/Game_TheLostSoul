540cfb6a970c93d1cb78c124b5cc325a39e14da4
DIzzy061 <chaliedik9@gmail.com>
2025-06-13 22:41:54 +0700

    feat: implemented environment interaction (platforms, ladders, slopes)

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,29 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour
+public class Door : InteractableObject
 {
-
     [SerializeField] private Sprite closedSprite;
     [SerializeField] private Sprite openSprite;
-
     public Collider2D transitionCollider;
 
     private SpriteRenderer spriteRenderer;
     private Collider2D col;
+    private bool isOpen = false;
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         col = GetComponent<Collider2D>();
-
-        // По умолчанию дверь закрыта
         Close();
+    }
+
+    protected override void OnInteract()
+    {
+        if (isOpen)
+            Close();
+        else
+            Open();
     }
 
     public void Open()
     {
+        isOpen = true;
         if (spriteRenderer != null) spriteRenderer.sprite = openSprite;
         if (col != null) col.isTrigger = true;
         if (transitionCollider != null) transitionCollider.enabled = true;
@@ -31,6 +35,7 @@ public class Door : MonoBehaviour
 
     public void Close()
     {
+        isOpen = false;
         if (spriteRenderer != null) spriteRenderer.sprite = closedSprite;
         if (col != null) col.isTrigger = false;
         if (transitionCollider != null) transitionCollider.enabled = false;
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,12 +8,13 @@ public class Player: MonoBehaviour
     public static Player Instance { get; private set; }
 
     [SerializeReference] private float movingSpeed = 5f;
+    [SerializeField] private float interactionDistance = 1.5f;
+    [SerializeField] private LayerMask interactableLayer;
 
     private Rigidbody2D rb;
-
     private float minMovingSpeed = 0.1f;
-
     private bool isRunning = false;
+    private InteractableObject currentInteractable;
 
     private void Awake()
     {
@@ -21,18 +22,21 @@ public class Player: MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        CheckForInteractables();
+        HandleInteraction();
+    }
+
     private void FixedUpdate()
     {
         HandleMovement();
-
     }
 
     private void HandleMovement()
     {
         Vector2 inputVector = GameInput.Instance.GetMovementVector();
-
         inputVector = inputVector.normalized;
-
         rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
 
         if (Mathf.Abs(inputVector.x) > minMovingSpeed || Mathf.Abs(inputVector.y) > minMovingSpeed)
@@ -43,6 +47,38 @@ public class Player: MonoBehaviour
         {
             isRunning = false;
         } 
+    }
+
+    private void CheckForInteractables()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, interactionDistance, interactableLayer);
+        
+        InteractableObject closestInteractable = null;
+        float closestDistance = float.MaxValue;
+
+        foreach (Collider2D collider in colliders)
+        {
+            InteractableObject interactable = collider.GetComponent<InteractableObject>();
+            if (interactable != null)
+            {
+                float distance = Vector2.Distance(transform.position, collider.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestInteractable = interactable;
+                }
+            }
+        }
+
+        currentInteractable = closestInteractable;
+    }
+
+    private void HandleInteraction()
+    {
+        if (currentInteractable != null && Input.GetKeyDown(KeyCode.E))
+        {
+            currentInteractable.Interact();
+        }
     }
 
     public bool IsRunning()
